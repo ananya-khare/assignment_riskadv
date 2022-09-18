a5ce2c8a027afaa813ed6906817defbafe1603a8
Ananya Khare <ananya.khare19@st.niituniversity.in>
2022-09-18 21:02:43 +0530

    Updated Models

--- backend/equities/views.py
+++ backend/equities/views.py
@@ -1,17 +1,29 @@
-from http.client import HTTPResponse
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import csv
 import io
+from equities.models import Equity
 # Create your views here.
 @csrf_exempt
 def get_csv_file(request):
     if request.method == "POST":
-        file = io.TextIOWrapper(request.FILES['file'])
-        reader = csv.DictReader(file)
-        for row in reader:
-            print(row)
+        if 'filetype' in request.POST and request.POST['filetype'] == "equities":
+            file = io.TextIOWrapper(request.FILES['file'])
+            reader = csv.DictReader(file)
+            for row in reader:
+                if Equity.objects.filter(equity_id=row['id']).first():
+                    continue
+                else:
+                    equity = Equity(
+                        equity_id=row['id'],
+                        equity_name=row['name'],
+                        equity_desc=row['description']
+                    )
+                    equity.save()
+                    return HttpResponse("Added Equities")
+
+
         return HttpResponse('200')
     if request.method == "GET":
         return HttpResponse("Only Accepts POST Requests")
