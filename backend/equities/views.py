f2130731dfde3bbe610a0ce368ce95ad82d6f069
Ananya Khare <ananya.khare19@st.niituniversity.in>
2022-09-19 04:52:57 +0530

    process returns

--- backend/equities/views.py
+++ backend/equities/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import csv
 import io
-from equities.models import Equity
+from equities.models import Equity,returns
+import datetime
 # Create your views here.
 @csrf_exempt
 def get_csv_file(request):
@@ -21,7 +22,28 @@ def get_csv_file(request):
                         equity_desc=row['description']
                     )
                     equity.save()
-                    return HttpResponse("Added Equities")
+                return HttpResponse("Added Equities")
+        if 'filetype' in request.POST and request.POST['filetype'] == 'returns':
+            file = io.TextIOWrapper(request.FILES['file'])
+            reader = csv.DictReader(file)
+            for row in reader:
+                if not Equity.objects.filter(equity_id=row['equity_id']).first():
+                    
+                    continue
+                if returns.objects.filter(equity=Equity.objects.filter(equity_id=row['equity_id']).first(),date=datetime.datetime.strptime(row['date'],'%Y-%m-%d')).first():
+                    continue
+                returns_obj = returns(
+                    equity = Equity.objects.filter(equity_id=row['equity_id']).first(),
+                    open = row['open'],
+                    close = row['close'],
+                    day_return = row['returns'],
+                    date = datetime.datetime.strptime(row['date'],'%Y-%m-%d')
+                )
+                returns_obj.save()
+                print('Saved')
+            return HttpResponse("Added Returns")
+
+                    
 
 
         return HttpResponse('200')
